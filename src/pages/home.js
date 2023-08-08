0c3e0448ae4fa388a0d32d14a755a26c1ccf4ebf
lilyflowr <ibadiela@gmail.com>
2023-08-08 16:05:48 +0100

    corrections

--- src/pages/home.js
+++ src/pages/home.js
@@ -63,48 +63,59 @@ const Flags = () => {
 };
 const About = () => {
   return (
-    <>
-      <div className="container">
-        <div className="row d-flex justify-content-center justify-content-evenly my-5">
-          {" "}
-          <h3 className="text-center mb-4">Lorem ipsum dolor sit amet</h3>{" "}
-          <div className="card col-lg-3 col-md-3 col-sm-12">
-            <img src={mission} className="card-img" alt="..." />
-            <div className="card-img-overlay text-center">
+    <div className="container">
+    <div className="row d-flex justify-content-center justify-content-evenly my-5">
+      <h3 className="text-center mb-4">Lorem ipsum dolor sit amet</h3>
+      <div className="col-lg-3 col-md-3 col-sm-12 p-0">
+        <div className="position-relative">
+          <img src={mission} className="card-img" alt="..." style={{ objectFit: 'cover', height: '100%' }} />
+          <div className="card-img-overlay text-center p-0 d-flex align-items-center">
+            <div>
               <h5 className="card-title text-white">Mission</h5>
-              <p className="card-text text-white">
-                Lorem ipsum dolor sit amet, consectetur adipiscing lorem ipsum
-                dolor sit amet, consectetur adipiscing
+              <p className="card-text text-center text-white">
+                Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit.
               </p>
             </div>
           </div>
-          <div className="card col-lg-3 col-md-3 col-sm-12">
-            <img src={vision} className="card-img" alt="..." />
-            <div className="card-img-overlay text-center">
-              <h5 className="card-title text-white">Vision</h5>
-              <p className="card-text text-white">
-                Lorem ipsum dolor sit amet, consectetur adipiscing lorem ipsum
-                dolor sit amet, consectetur adipiscing
-              </p>
-            </div>
-          </div>
-          <div className="card col-lg-3 col-md-3 col-sm-12">
-            <img src={values} className="card-img" alt="..." />
-            <div className="card-img-overlay text-center">
-              <h5 className="card-title text-white">Values</h5>
-              <p className="card-text text-white">
-                Lorem ipsum dolor sit amet, consectetur adipiscing lorem ipsum
-                dolor sit amet, consectetur adipiscing
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="text-center">
-          {" "}
-          <GetStartedButton />
         </div>
       </div>
-    </>
+      <div className="col-lg-3 col-md-3 col-sm-12 p-0">
+        <div className="position-relative">
+          <img src={vision} className="card-img" alt="..." style={{ objectFit: 'cover', height: '100%' }} />
+          <div className="card-img-overlay text-center p-0 d-flex align-items-center">
+            <div>
+              <h5 className="card-title text-white">Vision</h5>
+              <p className="card-text text-white">
+                Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-lg-3 col-md-3 col-sm-12 p-0">
+        <div className="position-relative">
+          <img src={values} className="card-img" alt="..." style={{ objectFit: 'cover', height: '100%' }} />
+          <div className="card-img-overlay text-center p-0 d-flex align-items-center">
+            <div>
+              <h5 className="card-title text-white">Values</h5>
+              <p className="card-text text-white">
+                Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="text-center">
+      <GetStartedButton />
+    </div>
+  </div>
+  
+  
+  
+  
+  
+  
   );
 };
 
@@ -164,50 +175,51 @@ const OurSessions = () => {
     <>
       <br />
       <br />
-      <div className="container-fluid p-5 align-items-center" style={{ backgroundColor: '#FFFFFF' }}>
+      <div className="container-fluid  align-items-center" style={{ backgroundColor: '#FFFFFF' }}>
         <h3 className="text-center">Our Sessions</h3>
 
-        <div className="d-flex justify-content-center">
-          <div className="row align-items-center justify-content-center justify-content-lg-between my-5">
-            {/* Card 21*/}
-            <div className="card mb-2 col-lg-6 col-md-6 col-sm-12 col-12 order-lg-1 order-2">
-              <div className="row">
-                <div className="col-4">
-                  <div className="image-container">
-                    <img src={session} alt="" style={{ maxWidth: '50px', height: 'auto' }} />
-                  </div>
-                </div>
-                <div className="col-8">
-                  <div className="text-container">
-                    <h4>Weekday Classes</h4>
-                    <p>Monday - Thursdays</p>
-                    <p>9.00am - 12.00pm</p>
-                    <p>2.00pm - 5.00pm</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            {/* Card 2 */}
-            <div className="card mb-2 col-lg-6 col-md-6 col-sm-12 col-12 order-lg-2 order-1">
-              <div className="row">
-                <div className="col-4">
-                  <div className="image-container">
-                    <img src={session} alt="" style={{ maxWidth: '50px', height: 'auto' }} />
-                  </div>
-                </div>
-                <div className="col-8">
-                  <div className="text-container">
-                    <h4>Weekday Classes</h4>
-                    <p>Monday - Thursdays</p>
-                    <p>9.00am - 12.00pm</p>
-                    <p>2.00pm - 5.00pm</p>
-                  </div>
-                </div>
-              </div>
-            </div>
+<div className=" flex justify-content-center">
+  <div className="row  my-5">
+    {/* Card 1 */}
+    <div className="card mb-2 col-lg-6 col-md-6 col-sm-12 col-12 order-lg-1 order-2" style={{ marginRight: '0px' }}>
+      <div className="row">
+        <div className="col-4">
+          <div className="image-container">
+            <img src={session} alt="" style={{ maxWidth: '50px', height: 'auto' }} />
           </div>
         </div>
+        <div className="col-8">
+          <div className="text-container">
+            <h4>Weekday Classes</h4>
+            <p>Monday - Thursdays</p>
+            <p>9.00am - 12.00pm</p>
+            <p>2.00pm - 5.00pm</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    {/* Card 2 */}
+    <div className="card mb-2 col-lg-6 col-md-6 col-sm-12 col-12 order-lg-2 order-1" style={{ marginLeft: '0px' }}>
+      <div className="row">
+        <div className="col-4">
+          <div className="image-container">
+            <img src={session} alt="" style={{ maxWidth: '50px', height: 'auto' }} />
+          </div>
+        </div>
+        <div className="col-8">
+          <div className="text-container">
+            <h4>Weekday Classes</h4>
+            <p>Monday - Thursdays</p>
+            <p>9.00am - 12.00pm</p>
+            <p>2.00pm - 5.00pm</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 
         <div className="text-center">
           <GetStartedButton />
@@ -228,7 +240,7 @@ const OurTeam = () => {
         <div className="d-flex justify-content-center">
           <div className="row align-items-center justify-content-center justify-content-lg-between my-5">
             {/* Card 1 */}
-            <div className="card mb-2 col-lg-6 col-md-6 col-sm-12 col-12 order-lg-1 order-2">
+            <div className=" mb-2 col-lg-6 col-md-6 col-sm-12 col-12 order-lg-1 order-2">
               <div className="row">
                 <div className="col-12">
                   <div className="image-container">
@@ -245,7 +257,7 @@ const OurTeam = () => {
             </div>
 
             {/* Card 2 */}
-            <div className="card mb-2 col-lg-6 col-md-6 col-sm-12 col-12 order-lg-2 order-1">
+            <div className=" mb-2 col-lg-6 col-md-6 col-sm-12 col-12 order-lg-2 order-1">
               <div className="row">
                 <div className="col-12">
                   <div className="image-container">
